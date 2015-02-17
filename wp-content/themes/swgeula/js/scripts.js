3d2255dfbcd778fa5da40bd9449d91af080d7095
Rivki <rebecca@shneorweb.com>
2015-02-17 10:53:03 +0200

    17 02

---
+++ wp-content/themes/swgeula/js/scripts.js
@@ -0,0 +1,24 @@
+angular.module('appgeula', ['ngRoute'])
+.config(function($routeProvider, $locationProvider) {
+	$locationProvider.html5Mode(true);
+ 
+	$routeProvider
+	.when('/', {
+		templateUrl: myLocalized.partials + 'main.html',
+		controller: 'Main'
+	})
+	.when('/:ID', {
+		templateUrl: myLocalized.partials + 'content.html',
+		controller: 'Content'
+	});
+})
+.controller('Main', function($scope, $http, $routeParams) {
+	$http.get('wp-json/posts/').success(function(res){
+		$scope.posts = res;
+	});
+})
+.controller('Content', function($scope, $http, $routeParams) {
+	$http.get('wp-json/posts/' + $routeParams.ID).success(function(res){
+		$scope.post = res;
+	});
+});
