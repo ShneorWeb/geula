afabf9d31ad2ac05641f5d3e9ebf2f49eab56c49
Ofer Shapira <ofer@shneorweb.com>
2015-02-26 14:02:03 +0200

    fix small error

--- wp-content/themes/swgeula/functions.php
+++ wp-content/themes/swgeula/functions.php
@@ -1,14 +1,5 @@
 <?php
 
-function theme_styles() {
-
-	
-	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
-
-}
-add_action( 'wp_enqueue_scripts', 'theme_styles' );
-
-
 function my_scripts() {	
 
 	wp_enqueue_style( 'swgeula_style', get_template_directory_uri() . '/style.css');
