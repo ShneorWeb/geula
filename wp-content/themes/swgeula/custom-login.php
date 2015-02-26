bf73e5878f8ea86c813c516bd8c7247e585a69e3
Ofer Shapira <ofer@shneorweb.com>
2015-02-26 14:42:10 +0200

    added lines bottom of registration

--- wp-content/themes/swgeula/custom-login.php
+++ wp-content/themes/swgeula/custom-login.php
@@ -1,3 +1,8 @@
+/*
+Template Name: Custom_Login
+*/
+
+
 <?php include_once("header.php");?>
 
 
@@ -37,49 +42,54 @@ function signinCallback(authResult) {
 </script>
 
 
-	<div id="div-login">       
+<div class="col-md-12"> 
+    <div id="div-login">       
 
-		<h1><?php _e("Login with an exisiting account","swgeulatr");?></h1>
-		<h2><?php _e("enter your email address and your password","swgeulatr");?></h2>
+    		<h1><?php _e("Login with an exisiting account","swgeulatr");?></h1>
+    		<h2><?php _e("enter your email address and your password","swgeulatr");?></h2>
 
-<?php 
+    <?php 
 
-$login  = (isset($_GET['login']) ) ? $_GET['login'] : 0;  
-if ( $login === "failed" ) {  ?>
-    '<p class="login-error-msg"><strong><?php _e("ERROR:","swgeulatr");?></strong> <?php _e("Invalid username and/or password.","swgeulatr");?></p>  
-<?php } elseif ( $login === "empty" ) {  ?>
-    '<p class="login-error-msg"><strong><?php _e("ERROR:","swgeulatr");?></strong> <?php _e("Username and/or Password is empty","swgeulatr");?>.</p>
-<?php } elseif ( $login === "false" ) {  ?>
-    '<p class="login-error-msg"><strong><?php _e("ERROR:","swgeulatr");?></strong> <?php _e("You are logged out.","swgeulatr");?></p>  
-<?php }  
+    $login  = (isset($_GET['login']) ) ? $_GET['login'] : 0;  
+    if ( $login === "failed" ) {  ?>
+        '<p class="login-error-msg"><strong><?php _e("ERROR:","swgeulatr");?></strong> <?php _e("Invalid username and/or password.","swgeulatr");?></p>  
+    <?php } elseif ( $login === "empty" ) {  ?>
+        '<p class="login-error-msg"><strong><?php _e("ERROR:","swgeulatr");?></strong> <?php _e("Username and/or Password is empty","swgeulatr");?>.</p>
+    <?php } elseif ( $login === "false" ) {  ?>
+        '<p class="login-error-msg"><strong><?php _e("ERROR:","swgeulatr");?></strong> <?php _e("You are logged out.","swgeulatr");?></p>  
+    <?php }  
 
-$args = array(
-        'redirect' => home_url(), 
-        'form_id' => 'loginform-custom',
-        'label_username' => '',
-        'label_password' => '',                
-        'label_remember' => __( 'Remember Me','swgeulatr' ),
-        'label_log_in' => __( 'Log In','swgeulatr' ),
-        'remember' => true
-);
+    $args = array(
+            'redirect' => home_url(), 
+            'form_id' => 'loginform-custom',
+            'label_username' => '',
+            'label_password' => '',                
+            'label_remember' => __( 'Remember Me','swgeulatr' ),
+            'label_log_in' => __( 'Log In','swgeulatr' ),
+            'remember' => true
+    );
 
-wp_login_form($args); 
-?>
-<p><a href="<?php echo wp_lostpassword_url(); ?>" title="<?php esc_attr_e( 'Lost my password','swgeulatr' ); ?>"><?php _e( 'Lost my password','swgeulatr' ); ?></a></p>
+    wp_login_form($args); 
+    ?>
+    <p><a href="<?php echo wp_lostpassword_url(); ?>" title="<?php esc_attr_e( 'Lost my password','swgeulatr' ); ?>"><?php _e( 'Lost my password','swgeulatr' ); ?></a></p>
 
-<hr/>
+    <hr/>
 
-<h2 class="signin-google"><?php _e("Or sign in with your Google account", "swgeulatr");?></h2>
+    <h2 class="signin-google"><?php _e("Or sign in with your Google account", "swgeulatr");?></h2>
 
-<p>
-<span id="signinButton">
-  <span
-    class="g-signin"
-    data-callback="signinCallback"
-    data-clientid="1071363229202-e8nga4duksuc3vldrpm3guuikc9i82ae.apps.googleusercontent.com"
-    data-cookiepolicy="single_host_origin"
-    data-requestvisibleactions="http://schema.org/AddAction"
-    data-scope="https://www.googleapis.com/auth/plus.login">
-  </span>
-</span>
-</p>
+    <p>
+    <span id="signinButton">
+      <span
+        class="g-signin"
+        data-callback="signinCallback"
+        data-clientid="1071363229202-e8nga4duksuc3vldrpm3guuikc9i82ae.apps.googleusercontent.com"
+        data-cookiepolicy="single_host_origin"
+        data-requestvisibleactions="http://schema.org/AddAction"
+        data-scope="https://www.googleapis.com/auth/plus.login">
+      </span>
+    </span>
+    </p>
+</div>
+
+<div id="login-bottom-text"><span><?php echo _e("Is this your first time? ","swgeulatr"); ?></span> <span><a href="<?php echo wp_registration_url();?>"><?php echo _e("Create a new account","swgeulatr"); ?></a></span></div>
+</div>
