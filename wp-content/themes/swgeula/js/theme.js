ebd1d6aa3e24ca23ae2e9c8811523bfd1a37c1bd
erez <erez2613@gmail.com>
2015-03-18 14:19:48 +0200

    menu icons

--- wp-content/themes/swgeula/js/theme.js
+++ wp-content/themes/swgeula/js/theme.js
@@ -1,6 +1,14 @@
 (function($) {
  
  $(document).ready(function(){
+     
+     //menu icons
+     $('.tachometer').prepend('<i class="fa fa-tachometer"></i>');
+     $('.library').prepend('<i class="icon-library-icon"></i>');
+     $('.microphone').prepend('<i class="fa fa-microphone"></i>');
+     $('.bookmark').prepend('<i class="fa fa-bookmark"></i>');
+     $('.cog').prepend('<i class="fa fa-cog"></i>');
+     $('.question').prepend('<i class="fa fa-question-circle"></i>');
     
   //end of document).ready function
  });  
