108656660c8dae63fa4a1de9e658a9a88567e015
erez <erez2613@gmail.com>
2015-04-02 14:51:51 +0300

    finish with authors page

---
+++ wp-content/themes/swgeula/page-authors.php
@@ -0,0 +1,105 @@
+<?php
+/*
+Template Name: Authors Page
+*/
+get_header(); ?>	
+
+<div class="col-lg-12 col-md-12 archive_cont">	
+
+<div class="page-header">	
+				
+
+				<div class="header_category">	
+
+					    <div class="back_to_libary">
+                        
+                            <h1><?php the_title(); ?></h1>
+                          
+                        </div>
+				</div>
+                
+			</div>
+        
+			<div class="search_by">
+                    
+                <div class="search_text">
+                    <span class="glyphicon glyphicon-search" aria-hidden="true"></span>  
+                    <input type="search" placeholder="חיפוש">
+                </div>
+
+			</div>
+    
+            <div id="spinner"></div>				    	   	
+		
+			<div class="categories row">
+                
+                
+
+				<ul class="product_list" style="padding:0px;">
+
+					<?php
+                    
+      
+					if (!is_category() || is_single()) {
+                        
+                        $args = array(
+                                /* https://codex.wordpress.org/Function_Reference/get_users */
+                                    'blog_id'      => 1,
+                                    'role'         => 'Administrator',
+                                 );
+                                $blogusers = get_users( $args );
+                                // Array of stdClass objects.
+                                foreach ( $blogusers as $user ) :
+                    
+                    ?>
+
+                           
+							<div class="category_single col-lg-3 col-md-4 col-sm-6 col-xs-12 ">
+
+									<li class="category_square"> 
+                                        
+										
+										<div class="category_square_content">                                         
+
+											<div class="category_square_author">
+												<?php
+                        												    
+                                                    //TODO : image from ofer function
+												    echo '<div class="category_square_avatar">'. get_avatar( $user, 100 ) . '</div>'; 
+
+												    echo '<div class="author_des"><div class="category_square_author_name"><h2>' . $user->display_name . '</h2></div>';
+												    
+												    echo '<div class="category_square_author_subject">' .$user->subject. '</div>';?>
+                                                
+                                                  <?php echo '<div class="category_square_author_subject desc">' .$user->description. '</div>';?>
+                                                
+											    	<div class="category_square_number">
+														 <!-- 
+                                                        TODO: get the number of posts dynamcly
+                                                        -->
+                                                        38 שיעורים בספריה
+													</div>
+											</div>
+													
+
+											</div>
+
+                                        </div>
+									</li>
+							</div>
+
+						<?php 
+                       
+                        endforeach; } 
+        ?>
+				</ul>
+				</div>
+				
+			</div>
+        </div>
+    </div>
+
+</div>
+
+<?php get_footer(); ?>
+				
