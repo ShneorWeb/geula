23894d6330659e86ed6199a773d2b1ffcb501b49
erez <erez2613@gmail.com>
2015-05-07 16:35:14 +0300

    finish with my_lessons page

--- wp-content/themes/swgeula/inc/category_boxes.php
+++ wp-content/themes/swgeula/inc/category_boxes.php
@@ -135,7 +135,7 @@
                                                         ?>
     												<p>
                                                     <?php $short_description = get_category_meta('short_description', get_term_by('slug', $cat->cat_name, 'category'));
-                                                        echo substr( $short_description, 0,180); 
+                                                        echo substr( $short_description, 0,170); 
                                                         echo "...";
                                                         ?>
                                                     </p>
@@ -191,7 +191,7 @@
     		
                         
                         
-                        </div>
+                        
     									</li>
     							</div>
 
