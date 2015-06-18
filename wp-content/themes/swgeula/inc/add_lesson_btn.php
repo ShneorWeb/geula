b0db89d1b62256dd4fc5142a91d9aeeaa3c99941
erez <erez2613@gmail.com>
2015-06-18 16:46:03 +0300

    global css fixes and ltr fixes to page-scheduling

--- wp-content/themes/swgeula/inc/add_lesson_btn.php
+++ wp-content/themes/swgeula/inc/add_lesson_btn.php
@@ -9,22 +9,22 @@ if ( isset($catAddID) ) :
         </a>
 <?php } else  if ($catStatus==1 || $catStatus==0) {  ?>    
          <!-- remove -->    
-         <a href="javascript:void(0);" id="btn_rm_<?php echo $catAddID;?>" onclick="if (confirm('<?php _e('Are you sure you want to remove this category from your lessons?', 'swgeula');?>')) { removeFromMyLessons(<?php echo $catAddID;?>); <?php if( isset($bMyLessons) && ($bMyLessons===true) ) {?>hideCat(<?php echo $catAddID;?>,'<?php echo $NumberSpanID; ?>')<?php }?>}" title="<?php _e('remove from my lessons', 'swgeula');?>" alt="<?php _e('remove from my lessons', 'swgeula');?>" class="add_lesson_btn remove" style="color:<?php echo $color; ?>"   onMouseOut="this.style.color='<?php echo $color; ?>'" style="color:<?php echo $color; ?>">
+         <a href="javascript:void(0);" id="btn_rm_<?php echo $catAddID;?>" onclick="if (confirm('<?php _e('Are you sure you want to remove this category from your lessons?', 'swgeula');?>')) { removeFromMyLessons(<?php echo $catAddID;?>); <?php if( isset($bMyLessons) && ($bMyLessons===true) ) {?>hideCat(<?php echo $catAddID;?>,'<?php echo $NumberSpanID; ?>')<?php }?>}" title="<?php _e('remove from my lessons', 'swgeula');?>" alt="<?php _e('remove from my lessons', 'swgeula');?>" class="add_lesson_btn remove" style="color:<?php echo $color; ?>;border-color:<?php echo $color; ?>"   onMouseOut="this.style.color='<?php echo $color; ?>'" style="color:<?php echo $color; ?>">
             <i class="fa fa-star add"></i>
             <i class="fa fa-times remove"></i>
         </a>
         <!-- add -->    
-        <a style="display:none;" id="btn_add_<?php echo $catAddID;?>" href="javascript:void(0);" onclick="addToMyLessons(<?php echo $catAddID;?>);" title="<?php _e('add to my lessons', 'swgeula');?>" alt="<?php _e('add to my lessons', 'swgeula');?>" class="add_lesson_btn add"  onMouseOver="this.style.color='<?php echo $color; ?>'" onMouseOut="this.style.color='#b2bac2'">
+        <a style="display:none;" id="btn_add_<?php echo $catAddID;?>" href="javascript:void(0);" onclick="addToMyLessons(<?php echo $catAddID;?>);" title="<?php _e('add to my lessons', 'swgeula');?>" alt="<?php _e('add to my lessons', 'swgeula');?>" class="add_lesson_btn add"  onMouseOver="this.style.color='<?php echo $color; ?>';this.style.border='2px solid <?php echo $color; ?>'" onMouseOut="this.style.color='#b2bac2';this.style.border='2px solid #EAEDEF'">
             <i class="fa fa-star"></i>
         </a>  
 <?php } else { ?>                
         <!-- remove -->    
-         <a style="display:none;" id="btn_rm_<?php echo $catAddID;?>" href="javascript:void(0);" onclick="if (confirm('<?php _e('Are you sure you want to remove this category from your lessons?', 'swgeula');?>')) { removeFromMyLessons(<?php echo $catAddID;?>); <?php if( isset($bMyLessons) && ($bMyLessons===true) ) {?>hideCat(<?php echo $catAddID;?>,'<?php echo $NumberSpanID; ?>')<?php }?>}" title="<?php _e('remove from my lessons', 'swgeula');?>" alt="<?php _e('remove from my lessons', 'swgeula');?>" class="add_lesson_btn remove" style="color:<?php echo $color; ?>"   onMouseOut="this.style.color='<?php echo $color; ?>'" style="color:<?php echo $color; ?>">
+         <a style="display:none;" id="btn_rm_<?php echo $catAddID;?>" href="javascript:void(0);" onclick="if (confirm('<?php _e('Are you sure you want to remove this category from your lessons?', 'swgeula');?>')) { removeFromMyLessons(<?php echo $catAddID;?>); <?php if( isset($bMyLessons) && ($bMyLessons===true) ) {?>hideCat(<?php echo $catAddID;?>,'<?php echo $NumberSpanID; ?>')<?php }?>}" title="<?php _e('remove from my lessons', 'swgeula');?>" alt="<?php _e('remove from my lessons', 'swgeula');?>" class="add_lesson_btn remove" style="color:<?php echo $color; ?>;border-color:<?php echo $color; ?>"   onMouseOut="this.style.color='<?php echo $color; ?>'" style="color:<?php echo $color; ?>">
             <i class="fa fa-star add"></i>
             <i class="fa fa-times remove"></i>
         </a>
         <!-- add -->    
-        <a href="javascript:void(0);" id="btn_add_<?php echo $catAddID;?>" onclick="addToMyLessons(<?php echo $catAddID;?>);" title="<?php _e('add to my lessons', 'swgeula');?>" alt="<?php _e('add to my lessons', 'swgeula');?>" class="add_lesson_btn add"  onMouseOver="this.style.color='<?php echo $color; ?>'" onMouseOut="this.style.color='#b2bac2'">
+        <a href="javascript:void(0);" id="btn_add_<?php echo $catAddID;?>" onclick="addToMyLessons(<?php echo $catAddID;?>);" title="<?php _e('add to my lessons', 'swgeula');?>" alt="<?php _e('add to my lessons', 'swgeula');?>" class="add_lesson_btn add"  onMouseOver="this.style.color='<?php echo $color; ?>';this.style.border='2px solid <?php echo $color; ?>'"  onMouseOut="this.style.color='#b2bac2';this.style.border='2px solid #EAEDEF'">
             <i class="fa fa-star"></i>
         </a>                          
 <?php
