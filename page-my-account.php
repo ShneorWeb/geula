ddb7243eb07a22188491a216cb7eb65b1782acb5
Ofer Shapira <ofer@shneorweb.com>
2016-01-21 22:09:36 +0200

    Revert "Revert "removing plugin files""
    This reverts commit ea5d2ae75f32ec2e3e1a34a863f88b3228dad920.

--- page-my-account.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-/*
-Template Name: My-Account
-*/
-wp_safe_redirect( home_url( 'my-account/sign-in/' ) );
-?>
--- sidebar.php
+++
@@ -1,7 +0,0 @@
-<div class="sidebar">
-
-          <?php 
-          	wp_nav_menu( array( 'theme_location' => 'right-menu' ) );         	          	
-          ?>
-
-</div>
